20024a11ec2a07b07b591b5f28f6df2329305fe4
antgobar <antongb09@gmail.com>
2024-10-01 14:20:23 +0100

    test message

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -12,7 +12,7 @@ router = APIRouter(tags=["pages"])
 def get_index_page(request: Request, user: User = Depends(get_current_user)):
     return templates.TemplateResponse(
         "pages/home.html",
-        {"request": request, "user": user},
+        {"request": request, "user": user, "other_message": "is this working??"},
     )
 
 
